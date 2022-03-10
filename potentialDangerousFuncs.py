2344c96fd1e5eacdb66236810be59ae12345035c
rudehero <>
2022-03-09 17:40:41 -0800

    emulation code runs.  emulation is erroring out gracefully, so it still succeeds in running entire script.  need to see if able to correct emulation difficulties

--- potentialDangerousFuncs.py
+++ potentialDangerousFuncs.py
@@ -3,7 +3,11 @@ from ghidra.app.decompiler import DecompileOptions
 from ghidra.app.decompiler import DecompInterface
 from ghidra.util.task import ConsoleTaskMonitor
 import ghidra.program.model.data
+import ghidra.pcode.emulate.EmulateExecutionState
 import re
+import struct
+#TODO -- Finalize emulation code; update to support 32 or 64 based on addr_size
+#cleanup all teh address factory calls
 
 #emulateIt = False
 emulateIt = True
@@ -36,6 +40,7 @@ bitness_masks = {
 try:
       addr_size = currentProgram.getMetadata()['Address Size']
       bitmask = bitness_masks[addr_size]
+      width = int(addr_size) // 8
 except KeyError:
       raise Exception("Unsupported bitness: {}. Add a bit mask for this target.".format(addr_size))
 
@@ -70,6 +75,8 @@ for func in funcs:
 # Step 2. Decompile all callers and find PCODE CALL operations leading to `target_add`
 options = DecompileOptions()
 monitor = ConsoleTaskMonitor()
+addrFactory = currentProgram.getAddressFactory()
+#emuMonitor = ConsoleTaskMonitor()
 ifc = DecompInterface()
 ifc.setOptions(options)
 ifc.openProgram(currentProgram)
@@ -147,20 +154,72 @@ for fkey in fkeys:
                             #set end breakpoint at the call of interest
                             emuHelper.setBreakpoint(op.getSeqnum().getTarget())
                             #fake return address to check in case we short circuit out
-                            bogusReturn = currentProgram.getAddressFactory().getAddress("0")
+                            bogusReturn = int("0x{}".format(addrFactory.getAddress("0")), 16)
                             #pull addr for callee func and ensure int format
                             calleeAddr = int("0x{}".format(call[0].getEntryPoint()), 16)
                             emuHelper.writeRegister(emuHelper.getPCRegister(), calleeAddr)
-
+                            print(emuHelper.readRegister(emuHelper.getPCRegister()))
+                            #set RBP and RSP values
+                            stackAddr = int("0x{}".format(currentProgram.getAddressFactory().getDefaultAddressSpace().getMaxAddress()), 16) - 0x7FFF
+                            emuHelper.writeRegister("RSP", stackAddr)
+                            stackOffset = 0
+                            emuHelper.writeStackValue(stackOffset, width, bogusReturn)
+                            stackOffset += width
+                            
                             #figure out params needed to supply...
                             numParams = call[0].getParameterCount()
                             params = call[0].getParameters()
                             for p in params:
                                 if(isinstance(p.getDataType(), ghidra.program.model.data.Pointer)):
-                                    emuHelper.writeRegister(p.getVariableStorage().getRegister(), int("0x{}".format(memAddr), 16))
+                                    if(p.getVariableStorage().isRegisterStorage()):
+                                        emuHelper.writeRegister(p.getVariableStorage().getRegister(), int("0x{}".format(memAddr), 16))
+                                    else:
+                                        #should be passed on stack
+                                        emuHelper.writeStackMemory(stackOffset, width, int("0x{}".format(memAddr), 16))
+                                        stackOffset += width
                                 else:
-                                    emuHelper.writeRegister(p.getVariableStorage().getRegister(), num)
+                                    if(p.getVariableStorage().isRegisterStorage()):
+                                        emuHelper.writeRegister(p.getVariableStorage().getRegister(), num)
+                                    else: 
+                                        emuHelper.writeStackMemory(stackOffset, width, num)
+                                        stackOffset += width
 
+                            #main emulation driver
+                            #while emuMonitor.isCancelled() == False:
+                            print("starting emulation run")
+                            while monitor.isCancelled() == False:
+                                succ = emuHelper.run(monitor)
+                                if(succ == False):
+                                    print("Emulation error: {}.  Aborting".format(emuHelper.getLastError()))
+                                    break
+                                currExecAddr = emuHelper.getExecutionAddress()
+                                if (currExecAddr == bogusReturn):
+                                    print("Emulation short circuited out of function.")
+                                    break
+                                if(currExecAddr == op.getSeqnum().getTarget()):
+                                    print("At the call!")
+                                    out += "Emulator derived param values\n"
+                                    out += "\tArg1: {}\tArg2: {}\tArg3: {}".format(emuHelper.readRegister("RDI"), emuHelper.readRegister("RSI"), emuHelper.readRegister("RDX"))
+                                    break
+                                '''
+                                emuStatus = emuHelper.getExecutionState()
+#ghidra.pcode.emulate.EmulateExecutionState
+                                #check status of the emulator
+                                #might actually be more than I need
+                                if(emuStatus == ghidra.pcode.emulate.EmulateExecutionState.BREAKPOINT):
+                                    #check address of breakpoint
+                                    if(currExecAddr == op.getSeqnum().getTarget()):
+                                        print("At the call!")
+                                        out += "Emulator derived param values\n"
+                                        out += "\tArg1: {}\tArg2: {}\tArg3: {}".format(emuHelper.readRegister("RDI"), emuHelper.readRegister("RSI"), emuHelper.readRegister("RDX"))
+                                        
+
+
+                                elif(emuStatus == ghidra.pcode.emulate.EmulateExecutionState.FAULT):
+                                    #check fault and take action
+                                elif(emuStatus == ghidra.pcode.emulate.EmulateExecutionState.STOPPED):
+                                    #honestly not sure what to do here...
+                            '''
                             #end emulation, clear out emuhelper
                             emuHelper.dispose()
 
